b9f83d86296dbcdb6588c35fb92d276b30257aa5
abr-ya <gamer05@list.ru>
2025-06-06 13:07:18 +0300

    feature: Update CoffeeCard component

--- src/components/CoffeeCard.tsx
+++ src/components/CoffeeCard.tsx
@@ -1,9 +1,30 @@
+import { Button, Card, Rate, Tag } from "antd";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+
 import type { ICoffee } from "../src/interfaces/coffee.interfaces";
 
 interface ICoffeeCard {
   data: ICoffee;
 }
 
-const CoffeeCard = ({ data }: ICoffeeCard) => <div>{data.name}</div>;
+const CoffeeCard = ({ data }: ICoffeeCard) => (
+  <Card
+    className="card"
+    hoverable
+    key={data.id}
+    cover={<img src={data.image} />}
+    actions={[
+      <Button icon={<ShoppingCartOutlined />} key={data.name}>
+        {data.price}
+      </Button>,
+    ]}
+  >
+    <Card.Meta title={data.name} description={data.subTitle} />
+    <Tag style={{ marginTop: "24px" }} color="purple">
+      {data.type}
+    </Tag>
+    <Rate defaultValue={data.rating} disabled allowHalf style={{ marginTop: "24px" }} />
+  </Card>
+);
 
 export default CoffeeCard;
